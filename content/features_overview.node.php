f347aa621a6eb621b80c5be87ad0b6a40b51cd94
David Trainer <david@minimalmedia.com>
2011-11-01 16:23:11 -0700

    add features node

---
+++ content/features_overview.node.php
@@ -0,0 +1,67 @@
+array(
+  'nid' => '15',
+  'type' => 'page',
+  'language' => '',
+  'uid' => '1',
+  'status' => '1',
+  'created' => '1294370459',
+  'changed' => '1294370459',
+  'comment' => '0',
+  'promote' => '0',
+  'moderate' => '0',
+  'sticky' => '0',
+  'tnid' => '0',
+  'translate' => '0',
+  'vid' => '26',
+  'revision_uid' => '1',
+  'title' => 'Features Overview',
+  'body' => '<p>Flexible Publishing Workflow</p><p>Taxonomy</p><p>User Registration and Profiles</p><p>Events Calendar</p><p>Blog</p><p>Contact Form</p><p>Photo Gallery</p><p>SEO</p>',
+  'teaser' => '<p>Flexible Publishing Workflow</p><p>Taxonomy</p><p>User Registration and Profiles</p><p>Events Calendar</p><p>Blog</p><p>Contact Form</p><p>Photo Gallery</p><p>SEO</p>',
+  'log' => '',
+  'revision_timestamp' => '1294370459',
+  'format' => '2',
+  'name' => 'admin',
+  'picture' => '',
+  'data' => 'a:1:{s:13:\"form_build_id\";s:37:\"form-4d44350a98a4a028cff6eb8b979ec0ba\";}',
+  'path' => 'content/features-overview',
+  'last_comment_timestamp' => '1294370459',
+  'last_comment_name' => NULL,
+  'comment_count' => '0',
+  'taxonomy' => array(
+    'tags' => array(
+      '1' => 'Documentation',
+    ),
+  ),
+  'files' => array(),
+  'cpn' => FALSE,
+  'page_title' => FALSE,
+  'nodewords' => array(
+    'metatags' => array(),
+  ),
+  'menu' => array(
+    'link_title' => '',
+    'mlid' => 0,
+    'plid' => 0,
+    'menu_name' => 'primary-links',
+    'weight' => 0,
+    'options' => array(),
+    'module' => 'menu',
+    'expanded' => 0,
+    'hidden' => 0,
+    'has_children' => 0,
+    'customized' => 0,
+    'parent_depth_limit' => 8,
+  ),
+  'export_display' => '$display = new ;
+$display->layout = \'\';
+$display->layout_settings = \'\';
+$display->panel_settings = \'\';
+$display->cache = \'\';
+$display->title = \'\';
+$display->content = array();
+$display->panels = array();
+$display->hide_title = PANELS_TITLE_FIXED;
+$display->title_pane = \'0\';
+',
+  '#_export_node_encode_object' => '1',
+)
