367bbc3cd4efbfcfdcd367b6435ec68a04c251e0
David Trainer <davetrainer@gmail.com>
2013-02-08 08:21:52 -0800

    Rename slider feature

---
+++ modules/cm_features/cm_rotator/theme/views-view-unformatted--cm-rotator.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file views-view-unformatted.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php flexslider_add('flexslider'); ?>
+<?php drupal_add_js("jQuery(document).ready(function () { jQuery('.flexslider').flexslider(); });", 'inline'); ?>
+
+<div class="flexslider">
+<ul class="slides">
+<?php foreach ($rows as $id => $row): ?>
+  <li>
+    <?php print $row; ?>
+  </li>
+<?php endforeach; ?>
+</ul>
+</div>
